b8d9c4681ed49bdc12e6c170c09ad420ec0e9583
Austin Melms <memyselfandlie@gmail.com>
2018-08-19 20:56:22 -0400

    testing testing testing

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-const Express = require('express');
-const router = Express.Router();
-const app = Express();
-const port = 80;
+const Express = require('express')
+const router = Express.Router()
+const app = Express()
+const port = 80
 const subD = require('express-subdomain')
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(subD('bugs', router))
 app.set('trust proxy', true)
 
-//Serve static files
-app.use('/', Express.static('./build/static'));
-
-router.get('/', async (req, res) => { // TODO: [Markdown?]
-  return res.send('hello?')
-})
-
-app.get('*', function (request, response){
-  response.sendFile(`${__dirname}/build/static/index.html`);
-})
+app.use('/', Express.static('./build/static'))
+router.get('/', Express.static('./src/bugs'))
 
 //api specific routes
-router.get('/', function(req, res) {
-  res.send('oh hello');
-});
+app.post('/submit', function(req, res) {
+  res.send('oh hello there')
+  console.log(req.body)
+})
 
-app.listen(port);
+app.get('*', function (request, response) {
+  response.sendFile(`${__dirname}/build/static/index.html`)
+})
+
+app.listen(port)
