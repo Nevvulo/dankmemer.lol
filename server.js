84cd36f076bf13da1e20d01d86faff9f2293322b
Austin Melms <memyselfandlie@gmail.com>
2018-08-19 20:35:06 -0400

    tests

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const Express = require('express');
+const router = Express.Router();
 const app = Express();
-const port = 3000;
+const port = 80;
+const subD = require('express-subdomain')
+
+app.use(subD('bugs', router))
+app.set('trust proxy', true)
 
 //Serve static files
 app.use('/', Express.static('./build/static'));
 
+router.get('/', async (req, res) => { // TODO: [Markdown?]
+  return res.send('hello?')
+})
+
 app.get('*', function (request, response){
   response.sendFile(`${__dirname}/build/static/index.html`);
 })
-/*
-app.use((req, res, next) => {
-  res.status(404).send("Sorry can't find that!")
+
+//api specific routes
+router.get('/', function(req, res) {
+  res.send('oh hello');
 });
-*/
+
 app.listen(port);
