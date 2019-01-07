397438eba69353a291471e4e629604cde367bf63
Austin Melms <memyselfandlie@gmail.com>
2019-01-06 22:03:49 -0500

    Feat: remove commands pushing

--- server.js
+++ server.js
@@ -9,18 +9,6 @@ const port = config.port || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//handle posting of commands to the server
-app.post('/api/cmds', (req, res) => {
-  if(keys.includes(req.headers.authorization)) {
-    commands = req.body.commands;
-    res.status(200).send({status: 'ok'});
-  }
-  else {
-    res.status(401).send({error: 'Get away you sick filth.'});
-  }
-  
-})
-
 app.get('/source', (req, res) => {
   res.status(200).sendfile('./source.zip');
 })
